90cd9772f3304377c26cd79ede6a0d3d28a36959
nickjvturner <nickjvturner@gmail.com>
2025-05-03 10:41:10 +0100

    created Dir Structure 3 to satisfy capitalisation requirement

---
+++ admin/dir_structure_profiles/Dir Structure 3.py
@@ -0,0 +1,21 @@
+directory_structure = {
+    'AP Location Maps': {},
+    'BoM': {
+        'Archive': {},
+    },
+    'Ekahau': {
+        'Archive': {},
+    },
+    'Floor Plans': {
+        'DWG': {},
+        'PDF': {},
+        'PNG': {},
+        'Archive': {}
+    },
+    'Notes': {},
+    'Pictures': {},
+    'Report': {
+        'Archive': {},
+    },
+    'Templates': {}
+}
